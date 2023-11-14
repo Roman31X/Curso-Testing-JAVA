b2fdbde4ca32c97ae4a150e77e364fc971762265
Roman31X <die.haa65@gmail.com>
2023-11-14 09:02:36 -0500

    CursoTesting

---
+++ src/main/java/com/roman31x/curso/testing/java/CursoTestingJAVA.java
@@ -0,0 +1,12 @@
+package com.roman31x.curso.testing.java;
+
+/**
+ *
+ * @author Roman31X
+ */
+public class CursoTestingJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
