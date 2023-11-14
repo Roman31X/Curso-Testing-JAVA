34b75c0af136368394818fc3de3588a2e021d00a
Roman31X <die.haa65@gmail.com>
2023-11-14 09:16:47 -0500

    test#1

---
+++ src/main/java/com/roman31x/curso/testing/java/Modulo01/TestingString.java
@@ -0,0 +1,13 @@
+package com.roman31x.curso.testing.java.Modulo01;
+
+public class TestingString {
+    /**
+     * Seleccionar el nombre de la clase [alt + enter] y seleccionar
+     * create Test y seleccionar la libreria JUNIT-4 posteriormente
+     * se creara una carpeta en la cual se podran realizar los test */
+
+    public String pruebaN1(String cadena, int numero){
+        return null;
+    }
+
+}
---
+++ src/test/java/com/roman31x/curso/testing/java/Modulo01/TestingStringTest.java
@@ -0,0 +1,7 @@
+package com.roman31x.curso.testing.java.Modulo01;
+
+import static org.junit.Assert.*;
+
+public class TestingStringTest {
+
+}
