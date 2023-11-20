a8161da1234b7832bcb8984e0eaf3d58af994262
Roman31X <die.haa65@gmail.com>
2023-11-19 21:36:30 -0500

    test#8

---
+++ src/main/java/com/roman31x/curso/testing/java/Modulo07/data/RepositorioPeliculas.java
@@ -0,0 +1,4 @@
+package com.roman31x.curso.testing.java.Modulo07.data;
+
+public class RepositorioPeliculas {
+}
---
+++ src/main/java/com/roman31x/curso/testing/java/Modulo07/service/ServicioPelicula.java
@@ -0,0 +1,4 @@
+package com.roman31x.curso.testing.java.Modulo07.service;
+
+public class ServicioPelicula {
+}
